0ea25675857778422024d8d12d94f2f99f79730a
Hung <tonic1307@gmail.com>
2023-02-07 02:06:20 +0700

    update Lap sec

--- pagerank_to_web.py
+++ pagerank_to_web.py
@@ -12,3 +12,33 @@ with client.read("/user/hadoop/output_ranks.csv", encoding='utf-8') as reader:
 pagerank_result['id'] = pagerank_result['float_id'].apply(int)
 pagerank_result['rank'] = pagerank_result['w'].rank(
     method='min', ascending=False).apply(int)
+
+with client.read("/user/hadoop/actors.csv", encoding='utf-8') as reader:
+    actors = pd.read_csv(reader)
+
+with client.read("/user/hadoop/popularity_weight.csv", encoding='utf-8') as reader:
+    weight = pd.read_csv(reader)
+
+weight.rename(columns={"actor1": "source", "actor2": "target"}, inplace=True)
+weight = weight[weight['source'].isin(
+    pagerank_result['id']) & weight['target'].isin(pagerank_result['id'])]
+
+weight['source'] = weight['source'].apply(str)
+weight['target'] = weight['target'].apply(str)
+
+data = pd.merge(actors, pagerank_result, on="id", how="inner")
+
+web_data = {'nodes': [], 'links': weight.to_dict('records')}
+
+for i, r in data.iterrows():
+    web_data['nodes'].append({
+        'id': str(r['id']),
+        'color': 'cyan' if r['gender'] == 2 else 'pink',
+        'size': 5*range(10, 0, -1)[r['rank']-1] if r['rank'] <= 10 else 1,
+        'popularity': int(r['popularity']),
+        'label': r['name'],
+        'rank': r['rank'],
+    })
+
+with open("data.json", "w") as outfile:
+    json.dump(web_data, outfile, indent=4)
